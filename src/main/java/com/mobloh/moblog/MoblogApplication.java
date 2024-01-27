27b97d6bd8828d05b9f282a10b27756271218d40
ashirbadpanda <ashirbadpanda6128@gmail.com>
2024-01-27 07:35:56 +0530

    Initial Commit

---
+++ src/main/java/com/mobloh/moblog/MoblogApplication.java
@@ -0,0 +1,13 @@
+package com.mobloh.moblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobloh/moblog/MoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobloh.moblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
