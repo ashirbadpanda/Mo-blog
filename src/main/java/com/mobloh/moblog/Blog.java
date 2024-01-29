18d501083d7eb66a07d5fffc0c675f88e5eee962
ashirbadpanda <ashirbadpanda6128@gmail.com>
2024-01-29 08:44:24 +0530

    backend logic implemented

---
+++ src/main/java/com/mobloh/moblog/Blog.java
@@ -0,0 +1,10 @@
+package com.mobloh.moblog;
+
+import lombok.Data;
+
+@Data
+public class Blog {
+    private String heading;
+    private String description;
+
+}
--- src/main/java/com/mobloh/moblog/BlogController.java
+++ src/main/java/com/mobloh/moblog/BlogController.java
@@ -1,25 +1,45 @@
 package com.mobloh.moblog;
 
+import java.io.PrintWriter;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
 @Controller
 public class BlogController {
+    private final List<Blog> blogs;
+
+    public BlogController(){
+        blogs = new ArrsyList<>();
+    }
+
+
     @GetMapping({"/","/blogs"})
-    public String blogs(){
+    public String blogs(Model model){
+        model.addAttribute("blogs",blogs);
         return "blogs";
     }
 
     @GetMapping("/blog")
-    public String blog() {
+    public String blog(@ModelAttribute int id, Model model) {
+        var blog = blogs.get(id);
+        model.addAttribute("blog", blog);
         return "blog";
     }
 
-    @GetMapping("/add-blog")
-    public String addblog() {
-        return "add-blog";
+    @PostMapping("/add-blog")
+    public void addBlog(@ModelAttribute Blog blog, PrintWriter pw) {
+        blogs.add(blog);
+        return "redirect:/blogs";
     }
     
 }
